9bd1f8c434b28614f73b4625003ea12ac080ebb5
Braden Hilton <bradenhilton@hotmail.co.uk>
2021-06-23 03:45:29 +0100

    Clean up tests to better organise multiple test cases for each function

--- cityhash_test.go
+++ cityhash_test.go
@@ -1,119 +1,154 @@
 package cityhash
 
 import (
+	"reflect"
 	"testing"
 )
 
-func TestCityHash64WithHelloWorld(t *testing.T) {
-	// input for CityHash64()
-	var (
-		input     []byte = []byte("Hello world")
-		inputSize uint32 = uint32(len(input))
-	)
-
-	// expected output hash value
-	const (
-		expectedOutput uint64 = 13182503335733000132
-	)
-
-	// output from CityHash64(input, inputSize)
-	var actualOutput uint64 = CityHash64(input, inputSize)
-
-	if actualOutput != expectedOutput {
-		t.Errorf("hash does not match expected output")
+func TestCityHash64(t *testing.T) {
+	type args struct {
+		s      []byte
+		length uint32
+	}
+	tests := []struct {
+		name string
+		args args
+		want uint64
+	}{
+		{
+			name: "Hello world",
+			args: args{
+				s:      []byte("Hello world"),
+				length: uint32(len([]byte("Hello world"))),
+			},
+			want: 13182503335733000132,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := CityHash64(tt.args.s, tt.args.length); got != tt.want {
+				t.Errorf("CityHash64() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
 
-func TestCityHash64WithSeedHelloWorld(t *testing.T) {
-	// input for CityHash64WithSeed()
-	var (
-		input     []byte = []byte("Hello world")
-		inputSize uint32 = uint32(len(input))
-		seed      uint64 = 8 //sizeof(uint64)
-	)
-
-	// expected output hash value
-	const (
-		expectedOutput uint64 = 4936126472499377365
-	)
-
-	// output from CityHash64WithSeed(input, inputSize, seed)
-	var actualOutput uint64 = CityHash64WithSeed(input, inputSize, seed)
-
-	if actualOutput != expectedOutput {
-		t.Errorf("hash does not match expected output")
+func TestCityHash64WithSeed(t *testing.T) {
+	type args struct {
+		s      []byte
+		length uint32
+		seed   uint64
+	}
+	tests := []struct {
+		name string
+		args args
+		want uint64
+	}{
+		{
+			name: "Hello world",
+			args: args{
+				s:      []byte("Hello world"),
+				length: uint32(len([]byte("Hello world"))),
+				seed:   8, //sizeof(uint64)
+			},
+			want: 4936126472499377365,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := CityHash64WithSeed(tt.args.s, tt.args.length, tt.args.seed); got != tt.want {
+				t.Errorf("CityHash64WithSeed() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
 
-func TestCityHash64WithSeedsHelloWorld(t *testing.T) {
-	// input for CityHash64WithSeeds()
-	var (
-		input     []byte = []byte("Hello world")
-		inputSize uint32 = uint32(len(input))
-		seed_1    uint64 = 1 //sizeof(uint8)
-		seed_2    uint64 = 8 //sizeof(uint64)
-	)
-
-	// expected output hash value
-	const (
-		expectedOutput uint64 = 10995333839364115670
-	)
-
-	// output from CityHash64WithSeeds(input, inputSize, seed_1, seed_2)
-	var actualOutput uint64 = CityHash64WithSeeds(input, inputSize, seed_1, seed_2)
-
-	if actualOutput != expectedOutput {
-		t.Errorf("hash does not match expected output")
+func TestCityHash64WithSeeds(t *testing.T) {
+	type args struct {
+		s      []byte
+		length uint32
+		seed0  uint64
+		seed1  uint64
+	}
+	tests := []struct {
+		name string
+		args args
+		want uint64
+	}{
+		{
+			name: "Hello world",
+			args: args{
+				s:      []byte("Hello world"),
+				length: uint32(len([]byte("Hello world"))),
+				seed0:  1, //sizeof(uint8)
+				seed1:  8, //sizeof(uint64)
+			},
+			want: 10995333839364115670,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := CityHash64WithSeeds(tt.args.s, tt.args.length, tt.args.seed0, tt.args.seed1); got != tt.want {
+				t.Errorf("CityHash64WithSeeds() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
 
-func TestCityHash128WithHelloWorld(t *testing.T) {
-	// input for CityHash128()
-	var (
-		input     []byte = []byte("Hello world")
-		inputSize uint32 = uint32(len(input))
-	)
-
-	// expected output hash values for uint128 first/lower and second/higher
-	const (
-		expectedOutputFirst  uint64 = 14676461730635999210
-		expectedOutputSecond uint64 = 18211519499011055500
-	)
-
-	// output from CityHash128(input, inputSize)
-	var (
-		actualOutput              = CityHash128(input, inputSize)
-		actualOutputFirst  uint64 = actualOutput.Lower64()
-		actualOutputSecond uint64 = actualOutput.Higher64()
-	)
-
-	if actualOutputFirst != expectedOutputFirst || actualOutputSecond != expectedOutputSecond {
-		t.Errorf("hash does not match expected output")
+func TestCityHash128(t *testing.T) {
+	type args struct {
+		s      []byte
+		length uint32
+	}
+	tests := []struct {
+		name string
+		args args
+		want Uint128
+	}{
+		{
+			name: "Hello world",
+			args: args{
+				s:      []byte("Hello world"),
+				length: uint32(len([]byte("Hello world"))),
+			},
+			want: Uint128{14676461730635999210, 18211519499011055500},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := CityHash128(tt.args.s, tt.args.length); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("CityHash128() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
 
-func TestCityHash128WithSeedWithHelloWorld(t *testing.T) {
-	// input for CityHash128WithSeed()
-	var (
-		input     []byte = []byte("Hello world")
-		inputSize uint32 = uint32(len(input))
-		seed             = Uint128{1, 8} // sizeof(uint8) & sizeof (uint64)
-	)
-
-	// expected output hash values for uint128 first/lower and second/higher
-	const (
-		expectedOutputFirst  uint64 = 15656991209745481333
-		expectedOutputSecond uint64 = 7719688212359498183
-	)
-
-	// output from CityHash128WithSeed(input, inputSize, seed)
-	var (
-		actualOutput              = CityHash128WithSeed(input, inputSize, seed)
-		actualOutputFirst  uint64 = actualOutput.Lower64()
-		actualOutputSecond uint64 = actualOutput.Higher64()
-	)
-
-	if actualOutputFirst != expectedOutputFirst || actualOutputSecond != expectedOutputSecond {
-		t.Errorf("hash does not match expected output")
+func TestCityHash128WithSeed(t *testing.T) {
+	type args struct {
+		s      []byte
+		length uint32
+		seed   Uint128
+	}
+	tests := []struct {
+		name string
+		args args
+		want Uint128
+	}{
+		{
+			name: "Hello world",
+			args: args{
+				s:      []byte("Hello world"),
+				length: uint32(len([]byte("Hello world"))),
+				seed:   Uint128{1, 8}, // sizeof(uint8) & sizeof (uint64)
+			},
+			want: Uint128{15656991209745481333, 7719688212359498183},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := CityHash128WithSeed(tt.args.s, tt.args.length, tt.args.seed); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("CityHash128WithSeed() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
